55f066aae0ab8cd2e0b5d31e069d9048b70e4fd4
Mblake59 <ablake59@hotmail.com>
2015-04-01 16:54:45 +0100

    Interfaces Done.

---
+++ PADIMapNoReduce/Interface/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PADIMapNoReduce
+{
+    public interface Client
+    {
+        // Worker calls to get a piece of the file
+        // WARNING: cliente precisa de passar linhas inteiras
+        byte[] getFileSplit(int startFile, int endFile);  
+
+        // Worker calls to give the cliente the processed split
+        bool receiveProcessedSplit( int split, byte[] file);
+
+        // JobTracker calls to tell the cliente the job is done
+        void jobDone();
+    }
+}
--- PADIMapNoReduce/Interface/JobTracker.cs
+++ PADIMapNoReduce/Interface/JobTracker.cs
@@ -8,5 +8,17 @@ namespace PADIMapNoReduce
 {
     public interface JobTracker
     {
+        // Client calls to give a job to the jobTracker
+        void startJob( int totalSplit, IMapper mapper, string clientURL, int totalBytes);
+
+        // Worker calls to get work from the JobTracker
+        // return is int[3] where 0 - start, 1 - end 2 - splitnumber
+        int[] getWork(int id); 
+
+        // Worker calls to tell he finished the job he had requested
+        void workDone(int id, int splitnumber);
+
+        // Prints the status to the console
+        void status();
     }
 }
