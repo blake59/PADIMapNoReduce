775cc121be3137b33130adfde9ecc1955eacc3b3
Mblake59 <ablake59@hotmail.com>
2015-04-01 16:38:52 +0100

    Repositorio Setup

---
+++ PADIMapNoReduce/PADIMapNoReduce/Worker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PADIMapNoReduce
+{
+    public interface Worker
+    {
+        // JobTracker calls to inform the worker he can ask for work
+        void workAvailable(string clientURL, IMapper mapper);
+
+        //  JobTracker calls to inform the worker that there is no more work
+        void workNotAvailable();
+
+        // prints to the console the status
+        void status();
+    }
+}
