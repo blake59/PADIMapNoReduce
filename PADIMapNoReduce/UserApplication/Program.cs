b3e603c6b3f5dbaa296f1f07b95364be13902dda
Mblake59 <ablake59@hotmail.com>
2015-04-04 16:27:18 +0100

    Client Done(Hopefully).

--- PADIMapNoReduce/UserApplication/Program.cs
+++ PADIMapNoReduce/UserApplication/Program.cs
@@ -3,14 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using PADIMapNoReduce;
+
+using System.Net;
+using System.Net.Sockets;
 
 namespace UserApplication
 {
     class Program
     {
+        // Returns IP ( not the street IP)
+        public static string getIP()
+        {
+            IPHostEntry host;
+            string localIP = "?";
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    localIP = ip.ToString();
+                }
+            }
+            return localIP;
+        }
+
+        // args <ENTRY-URL> <FILE> <OUTPUT> <S> <MAP> <DLL>
         static void Main(string[] args)
         {
+            if (args == null || args.Length < 6 )
+            {
+                Console.WriteLine("Error: Not Enough Args");
+                Console.ReadLine();
+                return;
+            }
+
+            string workerEntryUrl = args[0];
+            string filePath = args[1];
+            string outputPath = args[2];
+            int numberSplits = Int32.Parse(args[3]);
+            string mapClassName = args[4];
+            string dllPath = args[5];
+
+            UserApplication userApplication = new UserApplication(filePath, outputPath, numberSplits, mapClassName, dllPath);
+
+            userApplication.init(workerEntryUrl);
+
+            userApplication.submitJob();
+
+            Console.WriteLine("UserApplication: Job Done");
+            Console.ReadLine();
+
         }
+
+
     }
 }
--- PADIMapNoReduce/WorkerJobTracker/Program.cs
+++ PADIMapNoReduce/WorkerJobTracker/Program.cs
@@ -5,12 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 using PADIMapNoReduce;
 
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
 namespace WorkerJobTracker
 {
     class Program
     {
         static void Main(string[] args)
         {
+            TcpChannel clientChannel = new TcpChannel(30001);
+
+            ChannelServices.RegisterChannel(clientChannel,false);
+
+            RemotingConfiguration.RegisterWellKnownServiceType(
+            typeof(WorkerJobTracker), "W",
+            WellKnownObjectMode.Singleton);
+
             Console.WriteLine("Worker started");
             Console.ReadLine();
         }
