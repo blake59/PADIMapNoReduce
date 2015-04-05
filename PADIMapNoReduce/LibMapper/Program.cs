f364b7e629ae6124f7d931e9044b46b9d34ad9c7
Mblake59 <ablake59@hotmail.com>
2015-04-05 23:54:19 +0100

    a

--- PADIMapNoReduce/LibMapper/Program.cs
+++ PADIMapNoReduce/LibMapper/Program.cs
@@ -7,7 +7,7 @@ namespace LibMapper
     {
         public IList<KeyValuePair<string, string>> Map(string fileLine)
         {
-            System.Threading.Thread.Sleep(1000 * 5);
+            System.Threading.Thread.Sleep(1000);
             IList<KeyValuePair<string, string>> result = new List<KeyValuePair<string, string>>();
             result.Add(new KeyValuePair<string, string>("testKey1", fileLine));
             result.Add(new KeyValuePair<string, string>("testKey2", "anotherValue"));
