20e45f8e71f1199afa4412bc1ecb800c95d4eff3
marcocunha <marco.soudocunha@gmail.com>
2015-04-05 15:38:43 +0100

    PuppetMasterGUI
    PuppetMasterGUI done. Missing behaviour between GUI and the application

--- PADIMapNoReduce/PuppetMaster/Form1.Designer.cs
+++ PADIMapNoReduce/PuppetMaster/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PuppetMaster
 {
-    partial class Form1
+    partial class PuppetMasterForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,21 +28,166 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.puppetMasterTB = new System.Windows.Forms.TextBox();
+            this.puppetMasterUrlBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.commandTB = new System.Windows.Forms.TextBox();
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.loadScriptBtn = new System.Windows.Forms.Button();
+            this.commandsLbl = new System.Windows.Forms.Label();
+            this.infoLbl = new System.Windows.Forms.Label();
+            this.infoLB = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // Form1
+            // label1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PuppetMasterURL";
+            // 
+            // puppetMasterTB
+            // 
+            this.puppetMasterTB.Location = new System.Drawing.Point(104, 47);
+            this.puppetMasterTB.Name = "puppetMasterTB";
+            this.puppetMasterTB.Size = new System.Drawing.Size(212, 20);
+            this.puppetMasterTB.TabIndex = 1;
+            this.puppetMasterTB.TextChanged += new System.EventHandler(this.puppetMasterTB_TextChanged);
+            // 
+            // puppetMasterUrlBtn
+            // 
+            this.puppetMasterUrlBtn.Enabled = false;
+            this.puppetMasterUrlBtn.Location = new System.Drawing.Point(322, 45);
+            this.puppetMasterUrlBtn.Name = "puppetMasterUrlBtn";
+            this.puppetMasterUrlBtn.Size = new System.Drawing.Size(53, 23);
+            this.puppetMasterUrlBtn.TabIndex = 2;
+            this.puppetMasterUrlBtn.Text = "Apply";
+            this.puppetMasterUrlBtn.UseVisualStyleBackColor = true;
+            this.puppetMasterUrlBtn.Click += new System.EventHandler(this.puppetMasterUrlBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(44, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Command";
+            // 
+            // commandTB
+            // 
+            this.commandTB.Location = new System.Drawing.Point(104, 112);
+            this.commandTB.Name = "commandTB";
+            this.commandTB.Size = new System.Drawing.Size(212, 20);
+            this.commandTB.TabIndex = 4;
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.Location = new System.Drawing.Point(322, 109);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(53, 23);
+            this.submitBtn.TabIndex = 5;
+            this.submitBtn.Text = "Submit";
+            this.submitBtn.UseVisualStyleBackColor = true;
+            this.submitBtn.Click += new System.EventHandler(this.submitBtn_Click);
+            // 
+            // loadScriptBtn
+            // 
+            this.loadScriptBtn.Location = new System.Drawing.Point(104, 138);
+            this.loadScriptBtn.Name = "loadScriptBtn";
+            this.loadScriptBtn.Size = new System.Drawing.Size(212, 23);
+            this.loadScriptBtn.TabIndex = 6;
+            this.loadScriptBtn.Text = "Load Script";
+            this.loadScriptBtn.UseVisualStyleBackColor = true;
+            this.loadScriptBtn.Click += new System.EventHandler(this.loadScriptBtn_Click);
+            // 
+            // commandsLbl
+            // 
+            this.commandsLbl.AutoSize = true;
+            this.commandsLbl.BackColor = System.Drawing.SystemColors.Highlight;
+            this.commandsLbl.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.commandsLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.commandsLbl.Location = new System.Drawing.Point(144, 89);
+            this.commandsLbl.Name = "commandsLbl";
+            this.commandsLbl.Size = new System.Drawing.Size(96, 20);
+            this.commandsLbl.TabIndex = 7;
+            this.commandsLbl.Text = "Commands";
+            // 
+            // infoLbl
+            // 
+            this.infoLbl.BackColor = System.Drawing.SystemColors.Highlight;
+            this.infoLbl.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.infoLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoLbl.Location = new System.Drawing.Point(144, 181);
+            this.infoLbl.Name = "infoLbl";
+            this.infoLbl.Size = new System.Drawing.Size(96, 20);
+            this.infoLbl.TabIndex = 8;
+            this.infoLbl.Text = "Info";
+            this.infoLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // infoLB
+            // 
+            this.infoLB.FormattingEnabled = true;
+            this.infoLB.Location = new System.Drawing.Point(12, 204);
+            this.infoLB.Name = "infoLB";
+            this.infoLB.Size = new System.Drawing.Size(363, 82);
+            this.infoLB.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(144, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "General";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // PuppetMasterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(386, 298);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.infoLB);
+            this.Controls.Add(this.infoLbl);
+            this.Controls.Add(this.commandsLbl);
+            this.Controls.Add(this.loadScriptBtn);
+            this.Controls.Add(this.submitBtn);
+            this.Controls.Add(this.commandTB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.puppetMasterUrlBtn);
+            this.Controls.Add(this.puppetMasterTB);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "PuppetMasterForm";
+            this.Text = "PuppetMaster";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox puppetMasterTB;
+        private System.Windows.Forms.Button puppetMasterUrlBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox commandTB;
+        private System.Windows.Forms.Button submitBtn;
+        private System.Windows.Forms.Button loadScriptBtn;
+        private System.Windows.Forms.Label commandsLbl;
+        private System.Windows.Forms.Label infoLbl;
+        private System.Windows.Forms.ListBox infoLB;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- PADIMapNoReduce/PuppetMaster/Form1.cs
+++ PADIMapNoReduce/PuppetMaster/Form1.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 /*
@@ -18,16 +20,283 @@ using System.Diagnostics;
 */
 namespace PuppetMaster
 {
-    public partial class Form1 : Form
+    public partial class PuppetMasterForm : Form
     {
-        public Form1()
+        private static int LOG_MAX_LINES = 25;
+        private static string PUPPETMASTERURL = "192.169.1.1"; //TODO change this url
+        public PuppetMasterForm()
         {
             InitializeComponent();
+            puppetMasterTB.Text = PUPPETMASTERURL;
+            AddToLog("Welcome to PuppetMaster");
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void loadScriptBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Open Script File";
+            theDialog.Filter = "TXT files|*.txt";
+            theDialog.InitialDirectory = @"C:\";
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filename = theDialog.FileName;
+
+                string[] filelines = File.ReadAllLines(filename);
+
+                AddToLog("Run from script: " + filename);
+
+                //parse line by line
+                for (int a = 0; a < filelines.Length; a++)
+                {
+                    //Comment line
+                    if (filelines[a][0] == '%') continue;
+                    
+                    parseLine(filelines[a].Trim());
+                    
+                }
+
+                
+                //Test to see if it works
+                /*foreach (Employee emp in employeeList)
+                {
+                    MessageBox.Show(emp.EmployeeNum + Environment.NewLine +
+                        emp.Name + Environment.NewLine +
+                        emp.Address + Environment.NewLine +
+                        emp.Wage + Environment.NewLine +
+                        emp.Hours + Environment.NewLine);
+                }*/
+            }
+        }
+
+        private void parseLine(string line)
+        {
+            string[] splitLines = line.Split(' ');
+            switch (splitLines[0])
+            {
+                case "WORKER":
+                    if (splitLines.Length != 5)
+                        AddToLog("Invalid Command: " + line);
+                    else
+                        try
+                        {
+                          workerCmd(Convert.ToInt32(splitLines[1]), splitLines[2],
+                                    splitLines[3], splitLines[4]);
+                        }
+                        catch (FormatException e)
+                        {
+                            AddToLog("Invalid Command: " + line);
+                        }
+                    break;
+                case "SUBMIT":
+                    if (splitLines.Length != 6)
+                        AddToLog("Invalid Command: " + line);
+                    else
+                        try
+                        {
+                        submitCmd(splitLines[1], splitLines[2],
+                            splitLines[3], Convert.ToInt32(splitLines[4]), splitLines[5]);
+                        }
+                        catch (FormatException e)
+                        {
+                            AddToLog("Invalid Command: " + line);
+                        }
+                    break;
+                case "WAIT":
+                    if (splitLines.Length != 2)
+                        AddToLog("Invalid Command: " + line);
+                    else
+                        try{
+                        waitCmd(Convert.ToInt32(splitLines[1]));
+                        }
+                        catch (FormatException e)
+                        {
+                            AddToLog("Invalid Command: " + line);
+                        }
+                    break;
+                case "STATUS":
+                    if (splitLines.Length != 1)
+                        AddToLog("Invalid Command: " + line);
+                    else
+                        statusCmd();
+                    break;
+                case "SLOWW":
+                    if (splitLines.Length != 3)
+                        AddToLog("Invalid Command: " + line);
+                    else
+                        try { 
+                            slowwCmd(Convert.ToInt32(splitLines[1]), Convert.ToInt32(splitLines[2]));
+                        }
+                        catch (FormatException e)
+                        {
+                            AddToLog("Invalid Command: " + line);
+                        }
+                    break;
+                case "FREEZEW":
+                    if (splitLines.Length != 2)
+                        AddToLog("Invalid Command: " + line);
+                    else
+                        try { 
+                            freezewCmd(Convert.ToInt32(splitLines[1]));
+                        }
+                        catch (FormatException e)
+                        {
+                            AddToLog("Invalid Command: " + line);
+                        }
+                    break;
+                case "UNFREEZEW":
+                    if (splitLines.Length != 2)
+                        AddToLog("Invalid Command: " + line);
+                    else
+                        try { 
+                            unfreezewCmd(Convert.ToInt32(splitLines[1]));
+                        }
+                        catch (FormatException e)
+                        {
+                            AddToLog("Invalid Command: " + line);
+                        }
+                    break;
+                case "FREEZEC":
+                    if (splitLines.Length != 2)
+                        AddToLog("Invalid Command: " + line);
+                    else
+                        try { 
+                            freezecCmd(Convert.ToInt32(splitLines[1]));
+                        }
+                        catch (FormatException e)
+                        {
+                            AddToLog("Invalid Command: " + line);
+                        }
+                    break;
+                case "UNFREEZEC":
+                    if (splitLines.Length != 2)
+                        AddToLog("Invalid Command: " + line);
+                    else
+                        try { 
+                            unfreezecCmd(Convert.ToInt32(splitLines[1]));
+                        }
+                        catch (FormatException e)
+                        {
+                            AddToLog("Invalid Command: " + line);
+                        }
+                    break;
+                default:
+                    AddToLog("Invalid Command: "+line);
+                    break;
+            }
+        }
+
+
+        public void AddToLog(String message)    
+        {
+            // add this line at the top of the log
+            infoLB.Items.Insert(0, message);
+
+            // keep only a few lines in the log
+            while (infoLB.Items.Count > LOG_MAX_LINES)
+            {
+              infoLB.Items.RemoveAt(infoLB.Items.Count - 1);
+            }
+        }
+
+        private void submitBtn_Click(object sender, EventArgs e)
+        {
+            if (commandTB.Text != null)
+            {
+                AddToLog("Single command");
+                parseLine(commandTB.Text.Trim());
+                commandTB.Text = "";
+            }
+        }
+
+        /* PuppetMaster Available Commands */
+        private void workerCmd(int id, string puppetUrl, string serviceUrl, string entryUrl)
+        {
+            String command = "WORKER " + id + " " + puppetUrl + " " + serviceUrl + " " + entryUrl;
+            AddToLog(command);
+            //TODO add code here
+        }
+
+        private void submitCmd(string entryUrl, String file, String output, int splitsNumber, String mapClass)
+        {
+            String command = "SUBMIT " + entryUrl + " " + file + " " + output + " " + splitsNumber + " " + mapClass;
+            AddToLog(command);
+            
+            //TODO add code here
+        }
+
+        private void waitCmd(int secs)
+        {
+            String command = "WAIT " + secs;
+            AddToLog(command);
+            System.Threading.Thread.Sleep(1000 * secs);
+        }
+
+        private void statusCmd()
+        {
+            String command = "STATUS";
+            AddToLog(command);
+            //TODO add code here
+        }
+
+        private void slowwCmd(int id, int delay)
+        {
+            String command = "SLOWW " + id + " " + delay;
+            AddToLog(command);
+            
+            //TODO add code here
+        }
+
+        private void freezewCmd(int id)
+        {
+            String command = "FREEZEW " + id;
+            AddToLog(command);
+            
+            //TODO add code here
+        }
+
+        private void unfreezewCmd(int id)
+        {
+            String command = "UNFREEZEW " + id;
+            AddToLog(command);
+            
+            //TODO add code here
+        }
+
+        private void freezecCmd(int id)
+        {
+            String command = "FREEZEC " + id;
+            AddToLog(command);
+            
+            //TODO add code here
+        }
+
+        private void unfreezecCmd(int id)
+        {
+            String command = "UNFREEZEC " + id;
+            AddToLog(command);
+            
+            //TODO add code here
+        }
+
+        private void puppetMasterTB_TextChanged(object sender, EventArgs e)
+        {
+            puppetMasterUrlBtn.Enabled = true;
+        }
+
+        private void puppetMasterUrlBtn_Click(object sender, EventArgs e)
+        {
+            if (puppetMasterTB.Text != null && puppetMasterTB.Text != "")
+            {
+                PUPPETMASTERURL = puppetMasterTB.Text;
+                puppetMasterUrlBtn.Enabled = false;
+                AddToLog("PuppetMaster URL changed to: " + PUPPETMASTERURL);
+                //TODO add code here to change puppetmasterurl if it's possible
+            }
         }
     }
 }
